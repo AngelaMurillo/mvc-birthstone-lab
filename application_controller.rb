11d190428e28fafd55cc807ff560a555e0a2d810
Angela Murillo <findingangie@hotmail.com>
2017-06-13 20:05:14 +0000

    hello

--- application_controller.rb
+++ application_controller.rb
@@ -7,6 +7,11 @@ class MyApp < Sinatra::Base
   get '/' do
     erb :index
   end
+  post'/find_gem' do 
+   user_b_day = params[:birthday]
+   @gem = choose_gem(user_b_day)
+   erb :results
+  end 
 
   
   
--- models/model.rb
+++ models/model.rb
@@ -1,5 +1,32 @@
-def choose_gem(month)
-
+def choose_gem(date)
+    date= date.split("/")
+    month= date[0]
+    if month == "01"
+     return "Garnet"
+    elsif month == "02"
+     return "Amethyst"
+    elsif month == "03"
+     return "Aquamarine/Bloodstone"
+    elsif month == "04"
+     return "Diamond"
+    elsif month == "05"
+     return "Emerald"
+    elsif month == "06"
+     return "Pearl/Alexandrite/Moonstone"
+    elsif month == "07"
+     return "Ruby"
+    elsif month == "08"
+     return "Peridot"
+    elsif month == "09"
+     return "Sapphire"
+    elsif month == "10"
+     return "Tourmeline"
+    elsif month == "11"
+     return "Citrine/Yellow Topaz"
+    elsif month == "12"
+     return "Zircon/Turquoise"
+    end
+     
  
 end
 
